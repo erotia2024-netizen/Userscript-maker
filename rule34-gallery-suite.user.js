eba635a7fd1195accc9c653995cc2ef24ffe98f6
Erotia <erotia2024@gmail.com>
2026-09-20 19:57:29 +0200

    Rule34 Gallery Suite v0.8.43 desde el laboratorio

--- rule34-gallery-suite.user.js
+++ rule34-gallery-suite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rule34 Gallery Suite
 // @namespace    https://github.com/erotia2024-netizen/Userscript-maker
-// @version      0.8.42
+// @version      0.8.43
 // @description  Reconstruye rule34.xxx para PC: galeria escalable (tu eliges el tamano de miniatura) con icono de "ya visto", descarga de originales con nombre y carpeta propios (cola que se puede continuar y reintentar tras recargar), seleccion manual de posts (con lista de lo marcado) y lotes de una busqueda entera en un solo .zip, seccion de videos con barra de controles propia, analizador de etiquetas por personaje (con IA gratis sin configurar nada) que ademas prepara un prompt listo para pegar en cualquier app de imagen o video, aviso si hay otro descargador en conflicto, y panel "Mejoras" con todas las opciones del ensamblador, en espanol.
 // @author       rule34-gallery-suite
 // @homepageURL  https://github.com/erotia2024-netizen/Userscript-maker
@@ -97,7 +97,17 @@
     dlPages: "1",
     dlSkip: true,
     dlZip: false,
-    pdlAction: "ask"
+    pdlAction: "ask",
+    scOn: true,
+    scAuto: true,
+    scBadges: true,
+    scMode: "top",
+    scTop: 12,
+    scMin: 60,
+    scVideo: 6,
+    scSeen: true,
+    scHist: true,
+    scDup: true
   };
 
   function readJSON(key) {
