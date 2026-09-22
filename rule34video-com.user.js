4aa05a27da42ffd55ab337e4761aea68d3be6133
Erotia <erotia2024@gmail.com>
2026-09-22 23:27:25 +0200

    rule34video.com v0.1.52 desde el laboratorio

--- rule34video-com.user.js
+++ rule34video-com.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         rule34video.com
-// @version      0.1.51
+// @version      0.1.52
 // @description  Escrito en el laboratorio de Userscript Maker.
 // @author       Userscript Maker
 // @namespace    https://github.com/erotia2024-netizen/Userscript-maker
