aa554bfdead135760b79f81917988e0eec96a26a
Erotia <erotia2024@gmail.com>
2026-09-23 16:02:17 +0200

    h-flash.com v0.1.115 desde el laboratorio

--- h-flash-com.user.js
+++ h-flash-com.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         h-flash.com
-// @version      0.1.114
+// @version      0.1.115
 // @description  Escrito en el laboratorio de Userscript Maker.
 // @author       Userscript Maker
 // @namespace    https://github.com/erotia2024-netizen/Userscript-maker
