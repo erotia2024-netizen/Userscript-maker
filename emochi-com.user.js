64e45b76263cbf971d7ede3805e0cef82804d925
Erotia <erotia2024@gmail.com>
2026-09-26 03:27:40 +0200

    emochi.com v0.9.48 desde el laboratorio

--- emochi-com.user.js
+++ emochi-com.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         emochi.com
-// @version      0.9.47
+// @version      0.9.48
 // @description  Escrito en el laboratorio de Userscript Maker.
 // @author       Userscript Maker
 // @namespace    https://github.com/erotia2024-netizen/Userscript-maker
